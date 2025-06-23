8727a27a673c3ce8a0f5be962a27abe0a0e43781
SankeT DaS <shivam@Macbook.local>
2025-06-23 13:41:19 +0530

    Add PUT and DELETE features with UI updates

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); // enable method override
 
-// app.use(express.static('public'));
+let items = [];
 
-var app = express();
-app.set("view engine","ejs");
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
-var items = [];
-
-var example = "working";
-app.get("/",function(req,res){
-    res.render("list",{ejes:items});
+app.get("/", function (req, res) {
+    res.render("list", { ejes: items });
 });
 
-app.post("/",function(req,res){
-    var item = req.body.task;
+app.post("/", function (req, res) {
+    const item = req.body.task;
     items.push(item);
     res.redirect("/");
 });
 
-
-
-app.listen(4000,function(){
-    console.log("Server Started!");
+// PUT route to update a task
+app.put("/edit/:index", function (req, res) {
+    const idx = req.params.index;
+    items[idx] = req.body.updatedTask;
+    res.redirect("/");
 });
 
+// DELETE route to delete a task
+app.delete("/delete/:index", function (req, res) {
+    const idx = req.params.index;
+    items.splice(idx, 1);
+    res.redirect("/");
+});
+
+app.listen(4000, function () {
+    console.log("Server Started!");
+});
